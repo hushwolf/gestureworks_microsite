07e027faca543365e0db25bf746194f96786d651
Glass <glass@ideum.com>
2015-11-13 08:43:40 -0700

    add 400, 401, 404, 500 error pages

---
+++ 401.php
@@ -0,0 +1,171 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+
+	<title>401 Error - GestureWorks</title>
+
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no minimal-ui">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="robots" content="noindex,nofollow"> 
+		
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link href='https://fonts.googleapis.com/css?family=Lato:900,700,400,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,900,700,500,100' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body id="legal">
+
+	<header id="header">
+
+		<div id="header-topcontainer">
+
+			<div id="header-top-inner">
+
+				<ul>
+					<li>
+						<a class="headerlink vertcenter" href='http://gameplay3.gestureworks.com/' target="_blank">GAMEPLAY</a>
+					</li>
+					<li>
+						<a class="headerlink vertcenter" href='http://openexhibits.org/' target="_blank">OPEN EXHIBITS</a>
+					</li>
+				</ul>
+
+			</div><!-- /#header-top-inner -->
+
+		</div><!-- /#header-topcontainer -->
+
+		<div id="header-maincontainer">
+
+			<div id="gestureworks_icon" class="vertcenter">
+
+				<a href="/">
+					<img src="images/logo-gw-core.png">
+				</a>
+
+			</div><!-- /#gestureworks_icon -->
+
+			<div id="headerright" class="tablet tablet-n-desktop">
+
+				<ul id="main-navigation" class="vertcenter">
+					<li>
+						<a class="headerlink" href="http://gestureworks-hci.tumblr.com/" target="_blank">Blog</a>
+					</li>
+					<li>
+						<a class="headerlink" href="http://ideum.com/creative-services" target="_blank">Creative Services</a>
+					</li>
+					<li>
+						<a class="headerlink" href="http://wiki.gestureworks.com/doku.php" target="_blank">Wiki</a>
+					</li>
+				</ul>
+
+			</div><!-- /#headerright -->
+
+		</div><!-- /#header-maincontainer -->
+
+	</header>
+
+	<section id="mobile-navigation" class="phone">
+
+		<ul id="main-navigation" class="vertcenter">
+			<li>
+				<a class="headerlink" href="http://gestureworks-hci.tumblr.com/" target="_blank">Blog</a>
+			</li>
+			<li>
+				<a class="headerlink" href="http://ideum.com/creative-services" target="_blank">Creative Services</a>
+			</li>
+			<li>
+				<a class="headerlink" href="http://wiki.gestureworks.com/doku.php" target="_blank">Wiki</a>
+			</li>
+		</ul>
+
+	</section>
+
+	<section id="main-content" style="height:1000px;">
+
+		<div id="homepg" style="height:480px;">
+
+			<section id="legal_container">
+				<div id="legal_innercontainer">
+					<h1 class="headerone black">HTTP Error 401</h1>
+					<div class="paragraphone black">
+          
+	
+    
+    <section class="clearfix">
+    
+      <article class="clearfix">
+      
+        <div class="interior">
+                    
+          <p id="error">
+            <strong>Unauthorized Access</strong> - you have attempted to access a page for which you do not have sufficient permissions. 
+          </p>
+          
+          <p class="tablet-n-desktop">
+            Please try one or more of the following:
+          </p>
+          
+          <p>
+            If you typed the address, please make sure it is correct.<br>
+            Go to the <a href="/">Home page</a> and look for links that lead to the desired resource.<br>
+            Return to the <a href="javascript:history.go(-1)">last page</a> you were on and try again.<br>
+            Report the issue (with all pertinent details) through the <a 
+            href="http://ideum.com/contact/" target="_blank">Contact form</a>.
+          </ul>
+          
+          <p style="height:60px;">&nbsp;</p>
+          
+        </div>
+        
+      </article>    
+    
+    </section>
+			
+					</div><!-- /.paragraphone black -->
+			
+        </div><!-- /#legal_innercontainer -->
+
+			</section><!-- /#legal_container -->
+
+			<section id="software_support_container">
+				<div id="software_support_innercontainer2" class="vertcenter">
+					<h1 class="black">Software and Support</h1>
+					<p class="paragraphone black"><a href="http://ideum.com/contact/" target="_blank">Contact us</a> to learn more about GestureWorks Core software and support packages.</p> 
+				</div>
+				<div id="software_support_innercontainer3" class="vertcenter">
+					<p class="paragraphone black">
+						phone: (505) 792-1110</br>
+						toll free: (855) 898-6824</br>
+						fax: (505) 792-1111</br>
+						email: <a class="footerlink" href="mailto:sales@ideum.com?subject=Inquiry from the GestureWorks Web Site">sales@ideum.com</a>
+					</p>
+				</div>
+			</section>
+
+		</div><!-- /#homepg -->
+
+	</section>
+
+	<footer>
+
+		<ul id="footer" class="vertcenter">
+			<li class="footerlink white"><a href='http://gameplay3.gestureworks.com/' target="_blank">GAMEPLAY</a></li>
+			<li class="footerlink white"><a href='http://openexhibits.org/' target="_blank">OPEN EXHIBITS</a></li>
+			<li class="footerlink white"><a href="http://gestureworks-hci.tumblr.com/" target="_blank">BLOG</a></li>
+			<li class="footerlink white"><a href="http://ideum.com/creative-services" target="_blank">CREATIVE SERVICES</a></li>
+			<li class="footerlink white"><a href="http://wiki.gestureworks.com/" target="_blank">WIKI</a></li>
+			<li class="footerlink white"><a href="/">HOME</a></li>
+		</ul>
+
+		<p id="copyright">
+			&copy;Ideum - GestureWorks is a product of <a href="http://ideum.com">Ideum</a>
+		</p>
+
+	</footer>
+
+</body>
+
+</html>
